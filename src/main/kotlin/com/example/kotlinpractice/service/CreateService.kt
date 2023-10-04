1444d59a8704ef5ef50efc422b69faeaed6837b6
jangwooooo <s22018@gsm.hs.kr>
2023-10-04 10:51:34 +0900

    트랜잭션 어노테이션 추가

--- src/main/kotlin/com/example/kotlinpractice/service/CreateService.kt
+++ src/main/kotlin/com/example/kotlinpractice/service/CreateService.kt
@@ -3,6 +3,7 @@ package com.example.kotlinpractice.service
 import com.example.kotlinpractice.dto.AddPostRequestDto
 import com.example.kotlinpractice.entity.Post
 import com.example.kotlinpractice.repository.PostRepository
+import jakarta.transaction.Transactional
 import org.springframework.stereotype.Service
 
 @Service
@@ -10,6 +11,7 @@ class CreateService(
     private val postRepository: PostRepository
 ) {
 
+    @Transactional
     fun execute(addPostRequestDto: AddPostRequestDto) {
         val post: Post = Post(
             title = addPostRequestDto.title,
